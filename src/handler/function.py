43fdd6737b168e210ca5c4db5ade79f520c8c1de
Lev Litvinov <misery7100@gmail.com>
2022-11-20 23:48:11 +0300

    feat: handler <- sqs: dummy check

--- src/handler/function.py
+++ src/handler/function.py
@@ -1,9 +1,12 @@
-# placeholder lambda
-
 def lambda_handler(event, context):
 
     # some change
     x = 130222
+
+    print(event)
+    print(dir(event))
+    print(context)
+    print(dir(context))
     
     return {
         'statusCode': 200,
